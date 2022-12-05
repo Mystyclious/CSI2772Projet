823b0535e2f7e6c750619ce0834e17528886404e
Mystyclious <elliot.l.kirsch@gmail.com>
2022-12-05 17:12:52 -0500

    deck and discardpile

---
+++ DiscardPile.cpp
@@ -0,0 +1,67 @@
+#include "headers/DiscardPile.h"
+
+/**
+ * @brief  returns and removes the top card from the discard pile.
+ * 
+ * @return Card* 
+ */
+Card* DiscardPile::pickUp(){
+    Card* card;
+    card =  this->back();
+    this->pop_back();
+    return card;
+}
+
+/**
+ * @brief returns but does not remove the top card from the discard pile.
+ * 
+ * @return Card* 
+ */
+Card* DiscardPile::top(){
+    return this->back();
+}
+
+
+/**
+ * @brief permet d'inserer toutes les cartes du DiscardPile dans le ostream
+ * 
+ * @param os 
+ */
+void  DiscardPile::print(std::ostream& os){
+
+    for(int i = 0; i < this->size(); i++){
+        os << this->at(i)->getName()[0]<< " ";
+    };
+  
+}
+
+
+/**
+ * @brief insertion operator to display the discard pile object (only the card on top of the discard pile)
+ * 
+ * @param output 
+ * @param dp 
+ * @return std::ostream& 
+ */
+std::ostream& operator<<( std::ostream &output, const DiscardPile& dp ){
+
+    if(dp.size() > 0)
+       (dp.back())->print(output);
+    else 
+       output << "";
+
+    return output;
+}
+
+/**
+ * @brief write the discard pile inside a file
+ * 
+ * @param filename 
+ */
+void DiscardPile::saveDiscardPile(std::ofstream& filename){
+    for(int i = 0;  i < this->size() ; i++){
+        this->at(i)->saveCard(filename);
+        filename << std::endl;
+    }
+    std::cout << "Discard Pile saved." << std::endl;
+}
---
+++ Hand.cpp
@@ -0,0 +1,126 @@
+#include "headers/Hand.h"
+
+/**
+ * @brief returns and removes the top card from the player's hand.
+ * 
+ * @return Card* 
+ */
+Card* Hand::play(){
+    Card* card = pHand.front();
+    pHand.pop(); // remove the first element
+    return card;
+}
+
+/**
+ * @brief retourne la carte qui se trouve au sommet de la pile de la main (Hand)
+ * 
+ * @return Card* 
+ */
+Card* Hand::top(){
+    return pHand.front();
+}
+
+/**
+ * @brief retourne le nombre de cartes contenues dans la main (Hand)
+ * 
+ * @return int 
+ */
+int Hand::numCards(){
+    return pHand.size();
+}
+
+/**
+ * @brief retourne la carte à l'index spécifié par le paramètre pos
+ * 
+ * @param pos 
+ * @return Card* 
+ */
+Card* Hand::getCard(int pos){
+    Card* card = nullptr; // removed card to return
+    if(pos > pHand.size()-1){
+        std::cout << "(getCard) The index "  << pos << " can not be used. Current size of the hand = " << pHand.size() << std::endl;
+    }else{
+        std::queue <Card*, std::list<Card*>> temp; // temp player hand
+        Card* temp_card = nullptr;  // temp card
+        int find_idx = 0;
+        while(!pHand.empty()){
+
+            if(find_idx++ == pos){
+                // get the element
+                card = pHand.front();
+            }
+            temp_card = pHand.front(); // get the card
+            pHand.pop();     // add it in the temporary queue
+            temp.push(temp_card); // add the card in the queue
+            
+        }
+
+        // get the initial elements without the removed card inside pHand
+        while(!temp.empty()){
+            
+            temp_card = temp.front(); // get the card
+            temp.pop();     // add it in the temporary queue
+            pHand.push(temp_card); // add the card in the queue
+            
+        }
+    }
+
+   
+    return card;  
+}
+
+/**
+ * @brief insertion operator to display the content of the Hand object
+ * 
+ * @param output 
+ * @param hand 
+ * @return std::ostream& 
+ */
+std::ostream& operator<<( std::ostream& output, Hand& hand){
+
+    for( int pos = 0; pos < hand.numCards() ; pos++ ){
+        output << hand.getCard(pos) << std::endl;
+    }
+    return output;
+};
+
+
+/**
+ * 
+ * @brief write the card inside a file
+ * 
+ */
+void Hand::saveHand(std::ofstream& filename){
+
+    Card* card = nullptr; // removed card to return
+    std::queue <Card*, std::list<Card*>> temp; // temp player hand
+    Card* temp_card = nullptr;  // temp card
+    int find_idx = 0;
+    while(!pHand.empty()){
+        temp_card = pHand.front(); // get the card
+        temp_card -> saveCard(filename);
+        filename << std::endl;
+        pHand.pop();     // add it in the temporary queue
+        temp.push(temp_card); // add the card in the queue
+    }
+
+    // get the initial elements without the removed card inside pHand
+    while(!temp.empty()){
+        
+        temp_card = temp.front(); // get the card
+        temp.pop();     // add it in the temporary queue
+        pHand.push(temp_card); // add the card in the queue
+        
+    }
+
+    std::cout << "Hand saved." << std::endl;
+}
+
+/**
+ * @brief Get the List Of Cards inside the hand
+ * 
+ * @return std::queue <Card*, std::list<Card*>> 
+ */
+std::queue <Card*, std::list<Card*>> * Hand::getListOfCards(){
+    return &pHand;
+}
